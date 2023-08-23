492a48d695c54d153a05efe373500b4a4bcb7e50
sathwik reddy komandla <sathwikreddykomandla@gmail.com>
2023-08-24 00:32:09 +0530

    Fetch HTML (crawling for html ) from a website at a  url provided

--- crawl.js
+++ crawl.js
@@ -1,3 +1,59 @@
+const { createTestScheduler } = require('jest')
+const {JSDOM}=require('jsdom')
+
+
+
+async function crawlPage(currentURL){
+    console.log(`crawling ${currentURL}`)
+    try{
+        const resp=await fetch(currentURL)
+        if(resp.status>399){
+            console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
+            return
+        } const contentType=resp.headers.get("content-type")
+        if(!contentType.includes('text/html')){
+            console.log(`non html response with content type: ${contentType} on page ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+        
+    }catch(err){
+        console.log(`theres an error ${err.name} in the page ${currentURL}`)
+    }
+    }
+
+
+
+function getURLsFromHTML(htmlBody,baseURL){
+    const urls=[]
+    const dom=new JSDOM(htmlBody)
+    const linkElements=dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements){
+        console.log(linkElement.href)
+        if(linkElement.href.slice(0,1)=='/'){
+            //relative
+            try{
+                const urlObj=new URL(`${baseURL}${linkElement}`)
+                urls.push(`${baseURL}${linkElement}`)
+            }catch(err){
+                console.log("invlaid url ",err.name)
+            }
+        }else{
+        // absoulte
+        try{
+            const urlObj=new URL(linkElement.href)
+            urls.push(urlObj.href)
+        }catch(err){
+            console.log("invlaid url ",err.name)
+        }
+        }
+
+    }
+    return urls;
+
+}
+
+
 // crawl.js
 const normalizeUrl = urlString => {
     const urlObj=new URL(urlString)
@@ -10,4 +66,4 @@ const normalizeUrl = urlString => {
     }
 };
 
-module.exports= { normalizeUrl };
+module.exports= { normalizeUrl, getURLsFromHTML ,crawlPage};
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 // crawl.test.js
-const  { normalizeUrl } = require('./crawl.js') // Use the '.js' extension here
+const  { normalizeUrl, getURLsFromHTML } = require('./crawl.js') // Use the '.js' extension here
 
 const { test, expect } =require('@jest/globals');
 
@@ -29,5 +29,80 @@ test('normalizeUrl strip http', () => {
     const input = 'http://blog.GOOGLE.com/path/';
     const actual = normalizeUrl(input);
     const expected = 'blog.google.com/path';
+    expect(actual).toEqual(expected);
+});
+
+
+test('getURLsFromHTML absolute', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="https://blog.google.com/path/">
+            Google blog
+            </a>
+        </body>
+    </html>`;
+    const baseURL="https://blog.google.com";
+    const actual = getURLsFromHTML(input,baseURL);
+    const expected = ['https://blog.google.com/path/']
+    expect(actual).toEqual(expected);
+});
+
+
+
+
+test('getURLsFromHTML relative', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="/path/">
+            Google blog
+            </a>
+        </body>
+    </html>`;
+    const baseURL="https://blog.google.com";
+    const actual = getURLsFromHTML(input,baseURL);
+    const expected = ['https://blog.google.com/path/']
+    expect(actual).toEqual(expected);
+});
+
+
+
+test('getURLsFromHTML both absoulte and relative', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="https://blog.google.com/path/">
+            Google blog
+            </a>
+            <a href="/about/">
+            Google blog
+            </a>
+            <a href="/path/">
+            Google blog
+            </a>
+        </body>
+    </html>`;
+    const baseURL="https://blog.google.com";
+    const actual = getURLsFromHTML(input,baseURL);
+    const expected = ['https://blog.google.com/path/','https://blog.google.com/about/','https://blog.google.com/path/']
+    expect(actual).toEqual(expected);
+});
+
+
+
+
+test('getURLsFromHTML invalid url', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="invalid url">
+            Google blog
+            </a>
+        </body>
+    </html>`;
+    const baseURL="https://blog.google.com";
+    const actual = getURLsFromHTML(input,baseURL);
+    const expected = []
     expect(actual).toEqual(expected);
 });
